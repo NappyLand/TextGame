748151964b6d9472d1c0744b800937942e9bad29
o.o <home@oos-MacBook-Pro.local>
2017-11-23 21:41:41 -0600

    added search functions and clear function

--- textgame.pl
+++ textgame.pl
@@ -35,16 +35,16 @@ sub verifyinputstats{
 	return $_;
 }
 
-print "How smart are you?You have $statRemaining points left:";
+print "How smart are you? You have $statRemaining points left:";
 $intelligence = verifyinputstats();
 
-print "How dexterous are you?You have $statRemaining points left:";
+print "How dexterous are you? You have $statRemaining points left:";
 $dexterity = verifyinputstats();
 
-print "How much strength do you have?You have $statRemaining points left:";
+print "How much strength do you have? You have $statRemaining points left:";
 $strength = verifyinputstats();
 
-print "How lucky are you?You have $statRemaining points left:";
+print "How lucky are you? You have $statRemaining points left:";
 $luck = verifyinputstats();
 
 printstats();
@@ -56,18 +56,27 @@ print "You wake up in the forest, your clothing is missing besides your underwea
 print "You have a backpack.\n";
 print "You can search by typing the word search followed by what it is you want to look at ex. search backpack\n";
 print "press enter to move forth";
+
 sub searchcontainer{
 #for each element in the array you have passed to this sub, print each element
 foreach (@_){
 	print "$_\n";
 }
 }
-#searchcontainer(@backpack);
+
 if(my $input = <>){
 print $clear_string;
 }
-
-print "The sun is setting the city is in the distance about 6KM away\n";
+my $setting= "The sun is setting the city is in the distance about 6KM away\n";
+print "$setting";
+while (42){
 print "what do you do?\n>>";
 chomp(my $input = <>);
-print "$input";
+if ($input =~ /\Qsearch backpack\E/){
+
+	searchcontainer(@backpack)
+}
+if ($input =~/\Qclear\E/){
+	print $clear_string;
+}
+}
